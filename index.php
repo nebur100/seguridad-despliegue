baa2a34f776c2eeb0b22c37ba0e7a8fd0ffe1681
Ruben Moreno <165708572+nebur100@users.noreply.github.com>
2026-05-14 16:11:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 $host = 'db';
 $user = 'root';
-$pass = getenv('MARIADB_ROOT_PASSWORD'); // Aquí está el truco
+$pass = getenv('ROOT_PASSWORD');
 $db   = 'test_db';
 
+// Intentar conectar
 $conn = new mysqli($host, $user, $pass);
 
+// Verificar conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 
-echo "<h1>¡Despliegue automático de Rubén Moreno Riera funcionando al 100%!</h1>";
+echo "<h1>Despliegue automático de Rubén Moreno Riera funcionando al 100%!</h1>";
 echo "<p>Conexión a la base de datos establecida con éxito.</p>";
 ?>
